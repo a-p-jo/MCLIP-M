0616ac601cf632dfafd3d4556f682a397529f1f4
A.P. Jo <66941562+a-p-jo@users.noreply.github.com>
2021-07-14 16:28:58 +0530

    v2
    Line 36 : corrected str[len=1] to str[len-1]

--- src/mclipm.c
+++ src/mclipm.c
@@ -33,7 +33,7 @@ char * rm_newline(char * str)
 {
         size_t len = strlen(str);
         if(len>=2 && str[len-2] == '\r') str[len-2] = '\0';
-        else if(len>=1 && (str[len-1] == '\n' || str[len=1] == '\r') ) str[len-1] = '\0';
+        else if(len>=1 && (str[len-1] == '\n' || str[len-1] == '\r') ) str[len-1] = '\0';
         return str;
 }
 
